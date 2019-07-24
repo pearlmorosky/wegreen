8471dec608e00eeaf58fa4d566d2283717264b77
natali3z <Natalie.Zeakes@gmail.com>
2019-07-24 13:25:40 -0500

    Add files via upload

---
+++ DIY.js
@@ -0,0 +1,63 @@
+function LinkImg1(a) {
+  window.open("https://www.runnerduck.com/kc_plastic_birdhouse.htm");
+}
+function LinkImg2(b) {
+  window.open("https://designsbymke.blogspot.com/2012/01/lined-canvas-diaper-box.html");
+}
+function LinkImg3(c) {
+  window.open("https://www.recycle-eh.com/apps/blog/show/16934970-dust-scoop-from-plastic-milk-jug");
+}
+// function LinkImg4(d) {
+//   window.open("https://www.home-dzine.co.za/crafts/craft-cutlery-holder.htm");
+// }
+function LinkImg5(e) {
+  window.open("https://nuannaarpoq.wordpress.com/2011/10/22/household-hacks-coke-bottles/");
+}
+function LinkImg6(f) {
+  window.open("https://eighteen25.com/pretty-bathroom-jars/");
+}
+function LinkImg7(g) {
+  window.open("https://www.onegoodthingbyjillee.com/diy-mason-jar-dispenser/");
+}
+function LinkImg8(h) {
+  window.open("https://makeit-loveit.com/plastic-bottle-phone-holder-charger");
+}
+function LinkImg9(i) {
+  window.open("https://abeautifulmess.com/2012/08/gold-honey-bear-vase-diy-.html");
+}
+function LinkImg10(j) {
+  window.open("https://www.mymommystyle.com/50-easy-diy-projects-made-from-items-in-your-recycling-bin/");
+}
+function LinkImg11(k) {
+  window.open("https://www.thisoldhouse.com/ideas/10-uses-yogurt-cups");
+}
+function LinkImg12(l) {
+  window.open("https://maikonagao.blogspot.com/2013/02/diy-upcycled-plastic-bottle-herb-planter.html");
+}
+function LinkImg13(m) {
+  window.open("http://www.hangingwithmrshulsey.com/2017/07/diy-tire-seats.html");
+}
+function LinkImg14(n) {
+  window.open("https://riseofthegeeklings.com/2016/07/06/instant-hero-with-diy-hero-cuffs-kid-craft/");
+}
+function LinkImg15(o) {
+  window.open("https://www.hellowonderful.co/post/KID-MADE-DIY-RECYCLED-CARDBOARD-MARBLE-MAZE/#_a5y_p=5533746");
+}
+function LinkImg16(p) {
+  window.open("https://reusegrowenjoy.com/tic-tac-toe-on-the-go/");
+}
+function LinkImg17(q) {
+  window.open("https://www.craftsonsea.co.uk/cardboard-box-crafts-for-kids/");
+}
+function LinkImg18(r) {
+  window.open("https://www.craftandfun.com/2011/12/idee-di-natale-riciclo-bottiglie-di.html");
+}
+function LinkImg19(s) {
+  window.open("https://www.facebook.com/photo.php?fbid=268333409955985&set=a.245224355600224.52897.211476408975019&type=3&theater");
+}
+function LinkImg20(t) {
+  window.open("https://www.facebook.com/photo.php?fbid=323957007726958&set=a.245224355600224.52897.211476408975019&type=3&theater");
+}\
+function LinkImg21(u) {
+  window.open("https://www.facebook.com/photo.php?fbid=330333540422638&set=a.245224355600224.52897.211476408975019&type=3&theater");
+}
